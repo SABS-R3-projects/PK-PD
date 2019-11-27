4e437a560a3fe80becddbc96e7a7cb5d3bc1908a
rumneyr <rebecca.rumney@wolfson.ac.uk>
2019-11-27 10:56:08 +0000

    ::bug:: Removed the conflicting ABC inheritance from AbstractMainWindow, replacing it with QMainWindow.

--- PKPD/gui/abstractGui.py
+++ PKPD/gui/abstractGui.py
@@ -1,46 +1,39 @@
+from PyQt5.QtWidgets import QMainWindow
 from abc import ABC, abstractmethod
 
-class AbstractMainWindow(ABC):
+class AbstractMainWindow(QMainWindow):
     def __init__(self):
         # initialise window size, style, icon, windowname, tab structure
-        raise NotImplementedError
+        super().__init__()
 
-    @abstractmethod
     def _set_window_size(self):
         # set the default window size (make sure not fixed)
         raise NotImplementedError
 
-    @abstractmethod
     def _set_geometry(self):
         # set the window layout
         raise NotImplementedError
 
-    @abstractmethod
     def _set_style(self):
         # set style
         raise NotImplementedError
 
-    @abstractmethod
     def _set_icon(self):
         # st application icon
         raise NotImplementedError
 
-    @abstractmethod
     def _set_windowname(self):
         # set window name for top
         raise NotImplementedError
 
-    @abstractmethod
     def _set_tab_structure(self):
         # set the structure of tabs within the window
         raise NotImplementedError
 
-    @abstractmethod
     def _change_tab(self):
         # redraw to a different tab type
         raise NotImplementedError
 
-    @abstractmethod
     def _exit(self):
         # exit the application gracefully
         raise NotImplementedError
---
+++ PKPD/gui/mainWindow.py
@@ -0,0 +1,37 @@
+import sys
+from PKPD.gui.abstractGui import AbstractMainWindow
+from PyQt5.QtWidgets import QVBoxLayout, QMainWindow, QApplication
+from PyQt5.QtWidgets import QPushButton, QPlainTextEdit, QMenuBar, QStatusBar, QLabel
+
+
+class MainWindow(AbstractMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.top_bar = None
+        self.tab_view = None
+        self.bottom_bar = None
+        self.testUI()
+
+
+    def _set_geometry(self):
+        """Function which sets the layout of the main window"""
+        self.setMenuBar(self.top_bar)
+        self.setCentralWidget(self.tab_view)
+        self.setStatusBar(self.bottom_bar)
+
+    def testUI(self):
+        self.top_bar = QMenuBar()
+        self.top_bar.addMenu('File')
+        self.tab_view = QPlainTextEdit()
+        self.bottom_bar = QStatusBar()
+        version_number = QLabel('Version: 0.0.0')
+        self.bottom_bar.addWidget(version_number)
+        self._set_geometry()
+
+        self.setWindowTitle('PKPD')
+        self.showMaximized()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
