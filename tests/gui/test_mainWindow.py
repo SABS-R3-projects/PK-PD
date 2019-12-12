fcb6f0e9c90e3768c63ecbb76168e1c8ead8e1eb
rumneyr <rebecca.rumney@wolfson.ac.uk>
2019-12-12 16:55:13 +0000

    Attempting to get the testing working on Travis

--- tests/gui/test_mainWindow.py
+++ tests/gui/test_mainWindow.py
@@ -4,26 +4,26 @@ from PKPD.gui import mainWindow as mw, abstractGui as abs
 from PyQt5.QtWidgets import QPlainTextEdit, QLabel, QVBoxLayout, QApplication
 
 
-class Test_ui(unittest.TestCase):
+class Test_ui():
     def test_ui(self, visualise=False):
         """Sees whether it is possible to build the main window"""
-        # app = QApplication(sys.argv)
-        # window = mw.MainWindow()
-        #
-        # window.top_bar.addMenu('File')
-        # tab1, tab2 = TestTab("Model"), TestTab("Simulation")
-        # window.tabs = [tab1, tab2]
-        # version_number = QLabel('Version: 0.0.0')
-        # window.bottom_bar.addWidget(version_number)
-        # window._set_geometry()
-        # window.setWindowTitle('PKPD')
+        app = QApplication(sys.argv)
+        window = mw.MainWindow()
+
+        window.top_bar.addMenu('File')
+        tab1, tab2 = TestTab("Model"), TestTab("Simulation")
+        window.tabs = [tab1, tab2]
+        version_number = QLabel('Version: 0.0.0')
+        window.bottom_bar.addWidget(version_number)
+        window._set_geometry()
+        window.setWindowTitle('PKPD')
 
         if visualise:
             window.showMaximized()
             sys.exit(app.exec_())
         else:
-            self.assertTrue(True, msg=None)
-            # app.quit()
+            # self.assertTrue(True, msg=None)
+            app.quit()
 
 
 class TestTab(abs.AbstractTab):
